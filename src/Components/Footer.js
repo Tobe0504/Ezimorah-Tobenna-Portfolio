31eeab3ac67e5a7be585a10589c08dc4029500b2
tobe0504 <ezimorahtobenna@gmail.com>
2022-04-15 08:49:47 +0100

    Added some changed to the button styles, font and footer on mobile

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,15 +10,26 @@ const Footer = () => {
         <SocialIcon
           url="https://mobile.twitter.com/volfgng"
           bgColor="#21243D"
+          className={classes.icons}
+          style={{ height: 42, width: 42 }}
         />
         <SocialIcon
           url="https://www.linkedin.com/in/tobenna-ezimorah-51b497201"
           bgColor="#21243D"
+          className={classes.icons}
+          style={{ height: 42, width: 42 }}
         />
-        <SocialIcon url="https://github.com/Tobe0504" bgColor="#21243D" />
+        <SocialIcon
+          url="https://github.com/Tobe0504"
+          bgColor="#21243D"
+          className={classes.icons}
+          style={{ height: 42, width: 42 }}
+        />
         <SocialIcon
           url="https://instagram.com/bbvolfgng?utm_medium=copy_link"
           bgColor="#21243D"
+          className={classes.icons}
+          style={{ height: 42, width: 42 }}
         />
       </div>
       <p>Copyright &copy;2020. All rights reserved</p>
